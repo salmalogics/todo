d2fe634091aa5f5007ee4ba6658863ecda9833ea
unknown <salmamehrin1997@gmail.com>
2022-09-25 23:53:35 +0530

    files added

---
+++ todo.js
@@ -0,0 +1,58 @@
+
+        let task=document.getElementById('#input');
+        let todo=document.getElementById('#div2');
+        let addbutton=document.getElementById('#add');
+
+
+            // addbutton.addEventListener('click',function(){
+            //             var para=document.createElement("para");
+            //             para.innerText=task.value;
+            //             todo.appendChild(para); 
+            // });
+
+
+
+
+
+
+
+                                                                                       
+        $("#addbutton").click(function(){
+                   
+            var para=document.createElement("para");
+            para.innerText=task.value;
+            todo.appendChild(para); 
+            
+            
+            
+        });
+
+
+
+
+
+
+
+        // $(document).ready(function(){
+                 
+
+
+            
+                // $("#add").click(function(){
+                   
+                //     var para=document.createElement("para");
+                //     para.innerText=task.value;
+                //     todo.append(para); 
+                //     document.getElementById("div2").innerHTML="hi";
+                    
+                // });
+
+            // $("#para").dblclick(function(){
+            //     $("#para").hide();
+
+
+            // });   
+        // });                
+    // });
+
+   
